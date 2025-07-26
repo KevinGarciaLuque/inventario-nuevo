8853bb85ce51e6fb0e281d0be0317e5a122be588
KevinGarcia <109121035+KevinGarciaLuque@users.noreply.github.com>
2025-07-26 14:29:25 -0600

    Deploy1

--- frontend/src/components/BitacoraPage.jsx
+++ frontend/src/components/BitacoraPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api/axios";
 import { FaSearch, FaFilter, FaRedo } from "react-icons/fa";
-import "../styles/scroll.css";
+
 
 
 
